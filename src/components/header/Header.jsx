ed712eca97f61dafd8f756cd4e4f29f0686d0150
Sayan Dey <111982005+Sayan-97@users.noreply.github.com>
2022-10-31 18:23:22 +0530

    second commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import css from './header.module.css'
+
+const Header = () => {
+    return (
+        <div className={css.container}>
+            <div className={css.logo}>
+                logo
+            </div>
+            
+        </div>
+    )
+}
+
+export default Header
